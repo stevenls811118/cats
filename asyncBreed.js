7bed33bfcb1e6c0b2f9000dc3dbcb16abda5489a
Yifan Shi <stevenls1118@msn.com>
2022-10-18 22:46:26 -0600

    modified

--- asyncBreed.js
+++ asyncBreed.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone, errorFn) {
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // CHANGE: Pass data into callback instead of returning it directly
@@ -8,20 +8,20 @@ const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone, err
     if (!error) {
       functionToRunWhenThingsAreDone(data);
     } else {
-      errorFn();
+      functionToRunWhenThingsAreDone(undefined);
     }  
   });
 };
 
 // CHANGE 1: Moved the console.log into a new function:
 const printOutCatBreed = breed => {
-  console.log('Return Value: ', breed) // => print out details correctly.
+  console.log(breed) // => print out details correctly.
 };
 
-const errorFn = error => {
+/* const errorFn = error => {
   console.log("error to find this breed");
-};
+}; */
 // CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-breedDetailsFromFile('Bombay', printOutCatBreed, errorFn);
+breedDetailsFromFile('Bomb', printOutCatBreed);
 
 module.exports = breedDetailsFromFile;
--- test/asyncBreedsTest.js
+++ test/asyncBreedsTest.js
@@ -5,22 +5,14 @@ describe('#breedDetailsFromFile', () => {
   it('provides, via callback, breed details for the Bombay breed', (done) => {
     breedDetailsFromFile('Bombay', (bombay) => {
       const expectedDesc = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+      assert.equal(expectedDesc, bombay);
       done();
-      assert.equal(expectedDesc, bombay);  
-    }, (bombay) => {
-      console.log('Error to reading breed file')
-      done();
-      assert.equal(undefined, bombay);
     });
   });
-  it('provides, via callback, undefined for a breed that does not exist', (done, errorFn) => {
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
     breedDetailsFromFile('Saphire', (desc) => {
-      done();
-      assert.equal(undefined, desc); 
-    }, (desc) => {
-      console.log('Error to reading breed file')
-      done();
       assert.equal(undefined, desc);
-    });      
+      done();
+    });    
   });
 });
